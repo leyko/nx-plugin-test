d56ff7c6e4a476757164ee08b39cee53484d11ff
Julien <leyko54@gmail.com>
2024-03-15 19:02:32 +0100

    chore: fix version

--- libs/nx-cdk/src/utils/versions.ts
+++ libs/nx-cdk/src/utils/versions.ts
@@ -1,4 +1,4 @@
 export const awsCdkVersion = "2.122.0";
 export const awsCdkConstructsVersion = "10.3.0";
 export const awsCdkEslintVersion = "1.8.0";
-export const esbuildVersion = "1.8.0";
+export const esbuildVersion = "0.20.2";
